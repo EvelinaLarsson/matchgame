c4f75a4a6ba8a0e23915ff5569106b341be813ea
LeftEye <aevelinalarsson@gmail.com>
2017-11-22 22:05:43 +0000

    JS files

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,5 +1,10 @@
 var MatchGame = {};
 
+var card = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+var randomCard = [12, 1, 5, 9, 10, 0, 3, 11, 2, 15, 4, 13, 6, 7, 14, 8];
+console.log(randomCard);
+
+
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
